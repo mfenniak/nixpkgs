872306da1c41095307095cebbcaeb642f89f419b
Mathieu Fenniak <mathieu@fenniak.net>
2022-12-19 20:04:48 -0700

    WIP on getting the exporter systemd unit & tests running

--- nixos/modules/services/monitoring/prometheus/exporters/podman.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/podman.nix
@@ -9,6 +9,13 @@ in
   port = 9882;
   serviceOpts = {
     serviceConfig = {
+      DynamicUser = false;
+      StateDirectory = "prometheus-podman-exporter";
+      WorkingDirectory = "/var/lib/prometheus-podman-exporter";
+      # ProtectHome = lib.mkForce "tmpfs";
+      # WorkingDirectory = "$RUNTIME_DIRECTORY";
+      # RuntimeDirectory = "prometheus-podman-exporter";
+      # StateDirectory = "prometheus-podman-exporter";
       ExecStart = ''
         ${pkgs.prometheus-podman-exporter}/bin/prometheus-podman-exporter \
           --web.listen-address ${cfg.listenAddress}:${toString cfg.port} \
